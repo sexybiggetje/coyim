c4cc8af5d56fac0909222d7de44f5a34155b6399
Sofia Celi <sceli@thoughtworks.com>
2017-07-01 18:24:16 -0500

    Some details on first screen: set up

--- gui/definitions/first_account_dialog.go
+++ gui/definitions/first_account_dialog.go
@@ -14,17 +14,20 @@ func (*defFirstAccountDialog) String() string {
     <signal name="delete-event" handler="on_cancel_signal" />
     <child internal-child="vbox">
       <object class="GtkBox" id="Vbox">
-        <property name="homogeneous">True</property>
-	<property name="margin">10</property>
+        <property name="orientation">vertical</property>
+	<property name="margin">20</property>
+        <property name="margin_top">30</property>
+	<property name="spacing">2</property>
         <child>
           <object class="GtkLabel" id="message">
-            <property name="justify">center</property>
-            <property name="label" translatable="yes">Welcome to CoyIM!
-
-Since you do not have a configured account,
-we need to set up one.
-            </property>
             <property name="wrap">true</property>
+	    <property name="label" translatable="yes">Welcome to CoyIM!</property>
+            <property name="margin_bottom">10</property>
+            <attributes>
+              <attribute name="font-desc" value="&lt;Enter Value&gt; 14"/>
+              <attribute name="style" value="normal"/>
+              <attribute name="weight" value="semibold"/>
+            </attributes>
           </object>
           <packing>
             <property name="expand">false</property>
@@ -32,31 +35,49 @@ we need to set up one.
             <property name="position">0</property>
           </packing>
         </child>
+        <child>
+          <object class="GtkLabel">
+            <property name="wrap">true</property>
+	    <property name="label" translatable="yes">Since you do not have a configured account,
+we need to set up one.</property>
+          </object>
+          <packing>
+            <property name="expand">False</property>
+            <property name="fill">True</property>
+            <property name="position">2</property>
+          </packing>
+        </child>
         <child internal-child="action_area">
           <object class="GtkButtonBox" id="button_box">
             <property name="orientation">GTK_ORIENTATION_VERTICAL</property>
-            <child>
+            <property name="homogeneous">True</property>
+            <property name="margin_top">20</property>
+	    <child>
               <object class="GtkButton" id="button_register">
                 <property name="label" translatable="yes">Create a new account</property>
-                <signal name="clicked" handler="on_register_signal" />
+                <property name="margin_bottom">5</property>
+		<signal name="clicked" handler="on_register_signal" />
               </object>
             </child>
             <child>
               <object class="GtkButton" id="button_existing">
                 <property name="label" translatable="yes">Add an existing account</property>
-                <signal name="clicked" handler="on_existing_signal" />
+                <property name="margin_bottom">5</property>
+		<signal name="clicked" handler="on_existing_signal" />
               </object>
             </child>
             <child>
               <object class="GtkButton" id="button_import">
                 <property name="label" translatable="yes">Import account from your computer</property>
-                <signal name="clicked" handler="on_import_signal" />
+                <property name="margin_bottom">5</property>
+		<signal name="clicked" handler="on_import_signal" />
               </object>
             </child>
 	    <child>
               <object class="GtkButton" id="button_cancel">
                 <property name="label">Cancel</property>
-                <signal name="clicked" handler="on_cancel_signal" />
+                <property name="margin_bottom">5</property>
+		<signal name="clicked" handler="on_cancel_signal" />
               </object>
             </child>
           </object>
